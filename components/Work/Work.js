25a20dad6cc6fc345b7bfae8690ae5367654c1cb
Jen Rogers <jen@sci.utah.edu>
2021-10-31 18:13:59 -0600

    save

--- components/Work/Work.js
+++ components/Work/Work.js
@@ -14,7 +14,6 @@ import { faDesktop, faFilePdf, faPaperclip } from "@fortawesome/free-solid-svg-i
 
 const StyledWork = styled.div`
 
-
 width: 96%;
 
 height:50rem:
@@ -22,9 +21,7 @@ height:50rem:
 padding-right:10rem;
 margin-right: 1rem;
 
-padding-left: 2rem;
-
-
+padding-left: -1.5rem;
 
 a{
   text-decoration:none;
@@ -68,10 +65,12 @@ h1 {
       font-family:'Roboto',sans-serif;
       font-weight:300;
       width: 55rem;
+    
   }
   h3{
     display:block;
     font-weight:300;
+    font-size:1.9rem;
   }
 }
 `;
@@ -85,9 +84,11 @@ padding-right:10rem;
 margin-right: 1rem;
 padding-top: 10rem;
 padding-left: 2rem;
+padding-bottom:100px
 
 .cv-sub{
   width:50rem;
+  padding-left: 2rem;
 }
 
 p{
@@ -111,6 +112,7 @@ h2{
     font-family:'Roboto',sans-serif;
     font-weight:300;
     width: 55rem;
+    margin-left:2rem;
 }
 h3{
   display:block;
@@ -152,28 +154,22 @@ const Work = () => {
   return (
     <section className="Work">
           <CV>
-          <h1>CV</h1>
-          <div class="cv-sub">
+        
+          <div>
             <h2>Education</h2>
-            <div>
-              <h3>(2017 - Current) <b>Ph.D. Human-Centered Computing</b>, Scientific Computing and Imaging Institute, University of Utah </h3>
+            <div class="cv-sub">
+              <h3>(2017 - Current) Ph.D. Human-Centered Computing,</h3>
+              <p> Scientific Computing and Imaging Institute, University of Utah</p>
             </div>
             <div class="cv-sub">
-              <h3>(2015 - 2016) <b>MSc Medical Visualization and Human Anatomy</b>, Glasgow School of Art & University of Glasgow, With Distinction </h3>
+              <h3>(2015 - 2016) MSc Medical Visualization and Human Anatomy,</h3>
+              <p>Glasgow School of Art & University of Glasgow, With Distinction </p>
             </div>
             <div class="cv-sub">
-              <h3>(2010 - 2014) <b>BFA Graphic Design</b>, Montana State University, Highest Honors </h3>
+              <h3>(2010 - 2014) BFA Graphic Design,</h3>
+              <p>Montana State University, Highest Honors</p>
             </div>
-              {/* <li><b>Ph.D. Human-Centered Computing</b> (2017-current)
-                
-                </li>
-                <li>
-                <b>MSc Medical Visualization and Human Anatomy</b> (2015-2016)
-                Glasgow School of Art & University of Glasgow, With Distinction
-                </li>
-                <li><b>BFA Graphic Design</b>  (2010-2014)
-                Montana State University, Highest Honors</li>
-              </ul></p> */}
+       
           </div>
      
           <div>
@@ -181,39 +177,39 @@ const Work = () => {
           <StyledWork>
           <div class="cv-sub">
           <a href="https://animationlab.utah.edu/cova" target="blank"><Image work={Cova}/></a>
-          <h3>{Cova.title}</h3>
+          <h3>{Cova.title}  <a href={Cova.github}><FontAwesomeIcon icon={faGithub} /></a>
+          <a href={Cova.link} target = "blank"><FontAwesomeIcon icon={faDesktop} /></a></h3>
           <p dangerouslySetInnerHTML={{__html:Cova.text}}></p>
-          <a href={Cova.github}><FontAwesomeIcon icon={faGithub} size="2x"/></a>
-          <a href={Cova.link} target = "blank"><FontAwesomeIcon icon={faDesktop} size="2x"/></a>
+          {/* <a href={Cova.github}><FontAwesomeIcon icon={faGithub} size="2x"/></a>
+          <a href={Cova.link} target = "blank"><FontAwesomeIcon icon={faDesktop} size="2x"/></a> */}
           <p>Skills applied: requirements gathering, semi-structured interviews, mock-ups (Illustrator),
 web development (JavaScript, D3.js, HTML5, CSS), Python for image data processing</p>
         </div>
         <div class="cv-sub">
           <a href="https://vdl.sci.utah.edu/publications/2020_infovis_insights/" target="blank"><Image work={Trrrace}/></a>
-          <h3>{Trrrace.title}</h3>
+          <h3>{Trrrace.title}<a href={Trrrace.github}><FontAwesomeIcon icon={faGithub} /></a>
+          <a href={Trrrace.link} target = "blank"><FontAwesomeIcon icon={faDesktop} /></a>
+          <a href="https://vdl.sci.utah.edu/publications/2020_infovis_insights/" target="blank"><FontAwesomeIcon icon={faFilePdf}/></a></h3>
           <p>{Trrrace.text}</p>
-          <a href={Trrrace.github}><FontAwesomeIcon icon={faGithub} size="2x"/></a>
-          <a href={Trrrace.link} target = "blank"><FontAwesomeIcon icon={faDesktop} size="2x"/></a>
-          <a href="https://vdl.sci.utah.edu/publications/2020_infovis_insights/" target="blank"><FontAwesomeIcon icon={faFilePdf} size="2x"/></a>
+         
           <p>Skills applied: Python (NLTK, Gensim, Flask), TypeScript, React, Electron</p>
         </div>
         <div class="cv-sub">
           <a href={Trevo.link} target="blank"><Image work={Trevo}/></a>
-          <h3>{Trevo.title}</h3>
+          <h3>{Trevo.title} <a href={Trevo.github}><FontAwesomeIcon icon={faGithub} /></a>
+          <a href={Trevo.link} target = "blank"><FontAwesomeIcon icon={faDesktop} /></a>
+          <a href="https://vdl.sci.utah.edu/publications/2020_infovis_insights/" target="blank"><FontAwesomeIcon icon={faFilePdf}/></a></h3>
           <p dangerouslySetInnerHTML={{__html:Trevo.text}}></p>
-          <a href={Trevo.github}><FontAwesomeIcon icon={faGithub} size="2x"/></a>
-          <a href={Trevo.link} target = "blank"><FontAwesomeIcon icon={faDesktop} size="2x"/></a>
-          <a href="https://vdl.sci.utah.edu/publications/2020_infovis_insights/" target="blank"><FontAwesomeIcon icon={faFilePdf} size="2x"/></a>
+       
           <p>Skills applied: requirements gathering, semi-structured and contextual interviews,
-mock-ups (Illustrator), web development (JavaScript, D3.js, HTML5, CSS), development
-of interactive audit trail to trace back design study insights to underlying artifacts.</p>
+mock-ups (Illustrator), web development (JavaScript, D3.js, HTML5, CSS).</p>
         </div>
         <div class="cv-sub">
           <a href="https://vdl.sci.utah.edu/publications/2019_aci_composer/" target="blank"><Image work={Composer}/></a>
-          <h3>{Composer.title}</h3>
+          <h3>{Composer.title} <a href={Composer.github}><FontAwesomeIcon icon={faGithub} /></a>
+          <a href="https://vdl.sci.utah.edu/publications/2019_aci_composer/" target="blank"><FontAwesomeIcon icon={faFilePdf} /></a></h3>
           <p dangerouslySetInnerHTML={{__html:Composer.text}}></p>
-          <a href={Composer.github}><FontAwesomeIcon icon={faGithub} size="2x"/></a>
-          <a href="https://vdl.sci.utah.edu/publications/2019_aci_composer/" target="blank"><FontAwesomeIcon icon={faFilePdf} size="2x"/></a>
+         
           <p>Skills applied: requirements gathering, semi-structured interviews, mock-ups (Illustrator),
 web development (TypeScript, D3.js, HTML5, CSS), Python for data processing</p>
         </div>
@@ -248,14 +244,60 @@ that improve human interaction with assistive healthcare systems in the home.</p
             <div class="cv-sub"><h3>(2018, 2019) Teaching Mentor, University of Utah,</h3>
             <p>Visualization for Data Science (CS 6591). Organized and ran class labs. Designed and built
             visualization homework projects to teach Javascript and D3.</p>
+            </div>
             <div class="cv-sub"><h3>(2016-2017) Ambassador for Urban Uprising Foundation, Glasgow, UK</h3>
             <p>Mentor and coach for at risk youth from Ayrshire, UK to develop their foundational skills
             in climbing and progress toward their NIBAS certification. Organized and participated in
             sea-stack expedition in Northern Scotland to raise money and awareness for the charity.</p>
             </div>
             <div class="cv-sub"><p><b>* </b> When I am not coding, I am a climbing coach at the local climbing gym</p></div>
+          </div>
+
+          <div>
+            <h2>Peer-Reviewed Publications</h2>
+            <div class="cv-sub">
+              <p>Rogers, J., Patton, A. Harmon, L. Lex, A. Meyer, M
+                  Insights From Experiments With Rigor in an EvoBio Design Study
+                  IEEE Transactions on Visualization and Computer Graphics (InfoVis), 2020</p>
+              </div>
+            <div class="cv-sub">
+            <p>Rogers, J., Spina, N., Neese, A., Hess, R., Brodke, D. and Lex, A., 2019.
+Composer: Visual Cohort Analysis of Patient Outcomes.
+Applied clinical informatics, 10(02), pp.278-285.</p>
+            </div>
+            <div class="cv-sub">
+            <p>Rogers, J., Spina, N., Neese, A., Hess, R., Brodke, D. and Lex, A., 2018.
+Composer: Visual Cohort Analysis of Patient Outcomes
+Workshop on Visual Analytics in Healthcare at AMIA (VAHC 2018).</p>
+            </div>
+
+          </div>
+
+          <div>
+            <h2>Selected Talks + Presentations</h2>
+            <div class="cv-sub"><h3>(2021) Speaker, Invited Highlight Talk BioVis@ISMB 2021</h3>
+            <p>Presented work on tRRRace and traceabiility that came from work “Insights
+and Experiments with Rigor in an EvoBio Design Study”.</p>
+            </div>
+            <div class="cv-sub"><h3>(2018) Presenter, Workshop for VAHC, San Francisco, CA, United States</h3>
+            <p>Presented work on “Composer” interactive cohort analysis tool developed in
+collaboration with the Orthopaedic Research Center, University of Utah</p>
+            </div>
+            <div class="cv-sub"><h3>(2018) Poster Presenter IEEE VIS, Berlin, Germany</h3>
+            <p>Presented poster for interactive cohort analysis tool developed in collaboration with the
+Orthopaedic Research Center, University of Utah</p>
+            </div>
+            <div class="cv-sub"><h3>(2017) Poster Presenter and Student Grant Recipient, IS&T</h3>
+            <p>International Symposium on Electronic Imaging, Burlingame, California
+Invited to present interactive poster and speak on project developing interactive application
+to visualize research in motor imagery decoding.</p>
+            </div>
+            <div class="cv-sub"><h3>(2016) Invited Speaker Duke of Edinburgh Award Ceremony, Glasgow, UK</h3>
+            <p>Invited to speak to the attending body of the Duke of Edinburgh award ceremony on our
+climbing expedition to raise money and awareness for Urban Uprising Foundation.</p>
             </div>
           </div>
+
         </div>
         </CV>
         
